79e37005ca7e00ccfc50b4af5e953919449d5d24
ahmedm10 <ahmed.mohammed.m10@gmail.com>
2022-08-29 17:50:11 +0200

    rails7 upgrade changes

--- lib/datatables_crud.rb
+++ lib/datatables_crud.rb
@@ -6,32 +6,17 @@ def current_path
   File.dirname(__FILE__)
 end
 
-def include_file(filename)
-  filename = filename.sub(current_path, '')
-  require File.join(current_path, File.dirname(filename), File.basename(filename, '.rb'))
-end
-
-autoload_dirs = %w{datatables helpers support}
-
-autoload_dirs.each do |dir|
-  Dir.glob(File.join(current_path, 'datatables_crud', dir, '**/*.rb')).sort.each do |file|
-    include_file file
-  end
-end
-
 # load localizations
 Dir.glob(File.join(current_path, 'datatables_crud', 'config', 'locales', '**/*.yml')).each do |locale_file|
   I18n.load_path << locale_file
 end
 
 module DatatablesCRUD
-
   class Railtie < ::Rails::Railtie
-    initializer 'datatables_crud.init' do |app|
-      Dir.glob(File.join(current_path, 'datatables_crud', 'controllers', '**/*.rb')).sort.each do |file|
-        include_file file
-      end
+    configure do
+      ActiveSupport::Dependencies.autoload_paths << "#{__dir__}/datatables_crud/controllers"
+      ActiveSupport::Dependencies.autoload_paths << "#{__dir__}/datatables_crud/datatables"
+      ActiveSupport::Dependencies.autoload_paths << "#{__dir__}/datatables_crud/support"
     end
   end
-
 end
